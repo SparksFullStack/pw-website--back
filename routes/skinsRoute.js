a2d16d64a2201fd1de70cabd3eac577c4b951917
SparksFullStack <csparks@antioch.edu>
2019-03-21 21:14:43 -0700

    added a route that allows for adding new skins

--- routes/skinsRoute.js
+++ routes/skinsRoute.js
@@ -3,6 +3,18 @@ const SkinsModel = require('../models/SkinsModel');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('The skins router is live'));
+router.get('/', (req, res) => {
+
+});
+
+router.post('/add_skin', (req, res) => {
+    const { skin_image, skin_name, skin_link, buy_link, owners } = req.body;
+    const newSkin = new SkinsModel({ skin_image, skin_name, skin_link, buy_link, owners });
+
+    newSkin.save(err => {
+        if (err) return res.status(500).json({ addSkinError: 'There was an error creating the new skin, please try again' });
+        else res.status(200).json({ addSkinSuccess: "Skin successfully added!"});
+    });
+})
 
 module.exports = router;
